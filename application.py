83600b6fb73be4af37a0b5cb97f725431e54bde3
ganeshgunjal118@gmail.com <ganeshgunjal118@gmail.com>
2025-10-07 09:34:47 +0530

    files updated

--- application.py
+++ application.py
@@ -1,12 +1,21 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
 
+#import ridge regresso and standard scaler pickle files.
+ridge_model = pickle.load(open('models/ridge.pkl','rb'))
+standard_scaler = pickle.load(open('models/scaler.pkl','rb'))
 
 application = Flask(__name__)
 app = application
 
 @app.route('/')
-def hello_wold():
-    return '<h1>Hello Python World </h1>'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
